eafc86276e20095735ec35cdb518ef17528581f0
Alexander K <niktonezapomnil@gmail.com>
2022-02-25 18:02:14 +0500

    img fx

--- js/scripts.js
+++ js/scripts.js
@@ -257,7 +257,8 @@ $(document).ready(function () {
             infinite: true,
             speed: 300,
             slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToScroll: 1,
+            // adaptiveHeight: true
         });
     }
 
